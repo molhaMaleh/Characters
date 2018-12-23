72a6d54f66a0ff36b06de75f0ff095f93fa7017a
Molham Al-Maleh <molhamaleh@gmail.com>
2018-12-24 00:33:25 +0200

    feat
    Add Routing Module.
    Specify App Routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharacterComponent } from './character/character.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'characters', component: CharacterComponent },
+  { path: 'characters/:id', component: CharacterDetailComponent },
+  { path: '', redirectTo: '/characters', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
